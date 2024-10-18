40e8e293d496cfe38a8b4efdaff085790fac56f0
lyuzlion <2037178152@qq.com>
2024-10-18 20:25:18 +0800

    finish sleep

--- user/sleep.c
+++ user/sleep.c
@@ -3,10 +3,10 @@
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
-		fprintf(2, "usage: sleep <time>\n");
-		exit(1);
+		fprintf(2, "usage: sleep <number of ticks>\n"); // tick: 两次定时器中断的时间间隔。先进入到user/printf.c然后调用系统调用
+		exit(-1);
 	}
-	sleep(atoi(argv[1]));
+	sleep(atoi(argv[1])); // sleep 的实现位于 kernel/proc.c 该文件主要实现了进程管理相关的工作
 	exit(0);	
 }
 
