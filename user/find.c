cb05807fc61afdc6255ddfb65a03353ce8c3ae47
lyuzlion <2037178152@qq.com>
2024-10-19 00:57:57 +0800

    finish xargs

--- user/find.c
+++ user/find.c
@@ -5,37 +5,35 @@
 
 void find(char *path, char *target) {
     int fd;
-    if((fd = open(path, 0)) < 0) // RDONLY，打不开
-    {
-        printf("Error: cannot open %s\n", path);
+    if((fd = open(path, 0)) < 0) { // RDONLY，打不开
+        fprintf(2, "open %s error\n", path);
         return;
     }
-    struct stat st; // file status
-    if(fstat(fd, &st) < 0)
-    {
-        printf("Error: cannot stat %s\n", path);
+    struct stat st; // file status，包含文件所属的磁盘设备号、inode、文件类型、硬链接数、文件大小
+    if(fstat(fd, &st) < 0) {
+        fprintf(2, "get %s status error\n", path);
         close(fd);
         return;
     }
     
-    struct dirent directory_entry; // directory entry
+    struct dirent directory_entry; // directory entry，inum：inode number；name
     char path_tmp[512], *p;
     if(st.type == T_FILE){
         if(strcmp(path + strlen(path) - strlen(target), target) == 0) // 文件名匹配
             printf("%s\n", path);
     } else if(st.type == T_DIR){
-        if(strlen(path) + 1 + DIRSIZ + 1 > sizeof(path_tmp)){
-            printf("Error: path too long\n");
+        if(strlen(path) + DIRSIZ + 2 > sizeof(path_tmp)){ // '\0' + '/'
+            printf("path too long error\n");
             exit(0);
         }
         strcpy(path_tmp, path);
         p = path_tmp + strlen(path_tmp);
         *p++ = '/';
         while(read(fd, &directory_entry, sizeof(directory_entry)) == sizeof(directory_entry)) {
-            if(directory_entry.inum == 0) continue; // 未使用
-            memcpy(p, directory_entry.name, DIRSIZ);
+            if(directory_entry.inum == 0) continue; // 该目录项未使用
+            memcpy(p, directory_entry.name, DIRSIZ); // 把
             p[DIRSIZ] = 0;
-            if(strcmp(path_tmp + strlen(path_tmp) - 2, "/.") != 0 && strcmp(path_tmp + strlen(path_tmp) - 3, "/..") != 0) {
+            if(strcmp(path_tmp + strlen(path_tmp) - 2, "/.") != 0 && strcmp(path_tmp + strlen(path_tmp) - 3, "/..") != 0) { // 遇到. 和..不递归
                 find(path_tmp, target);
             }
         }
@@ -44,9 +42,13 @@ void find(char *path, char *target) {
 }
 
 int main(int argc, char *argv[]) {
+    if(argc != 3) {
+        fprintf(2, "usage : find <directory path> <name>\n");
+        exit(-1);
+    }    
     char target[512];
     target[0] = '/';
-    strcpy(target+1, argv[2]);
+    strcpy(target + 1, argv[2]);
     find(argv[1], target);
     exit(0);
 }
--- user/xargs.c
+++ user/xargs.c
@@ -1,60 +1,41 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/fs.h"
-#include "kernel/param.h"
 
-#define MAXN 1024
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]){
     if (argc < 2) {
         fprintf(2, "usage: xargs <command> [args...]\n");
         exit(1);
     }
-
-    char* argvs[MAXARG];
-    int index = 0;
-    for (int i = 1; i < argc; ++i) {
-        argvs[index++] = argv[i];
+    int arg_cnt = 0;
+    int cur = 0;
+    char c;
+    char buffer[32];
+    char *p = buffer;
+    char *arg_list[32];
+    for(int i = 1; i < argc; i++){
+        arg_list[arg_cnt++] = argv[i]; // 保存参数，包括要执行的命令以及命令的参数等。
     }
-    
-    char buf[MAXN] = {"\0"};
+    while(read(0, &c, sizeof(c)) > 0){
+        if(c == '\n'){
+            buffer[cur] = 0;
+            arg_list[arg_cnt++] = p;
 
-    int n;
-    char temp[MAXN];
-    // M: read the input from stdin, store it in buf
-    while ((n = read(0, buf, MAXN)) > 0) {
-        int j = 0;
-        for (int i = 0; i < n; ++i) {
-            // M: if the current character is '\n' or the last character in the buffer
-            if (buf[i] == '\n' || i == n - 1) {
-                if (buf[i] != '\n') {
-                    temp[j++] = buf[i];
-                }
-                // M: the two kinds of implementation are the same
-                temp[j] = 0;
-                // temp[j] = '\0';
-                if (index < MAXARG - 1) {
-                    // argvs[index] = strdup(temp);
+            p = buffer;
+            cur = 0;
+            arg_list[arg_cnt] = 0;
+            arg_cnt = argc - 1; // 记得要保留命令行里传入的参数
 
-                    // M: add the new argument to the argvs
-                    argvs[index] = temp; 
-                    argvs[index + 1] = 0; 
-                }
-                if (fork() == 0) {
-                    // M: only add one argument to the argvs a time
-                    exec(argv[1], argvs);
-                    exit(1);
-                }
-                wait(0);
-                j = 0;
-                memset(temp, 0, sizeof(temp));
-                // memset(temp, 0, MAXN);
-            } else {
-                temp[j++] = buf[i];
+            if(fork() == 0){
+                exec(argv[1], arg_list);
             }
+            wait(0);
+        }else if(c == ' ') {
+            buffer[cur++] = 0;
+            arg_list[arg_cnt++] = p;
+            p = &buffer[cur];
+        }else {
+            buffer[cur++] = c;
         }
     }
     exit(0);
 }
-
